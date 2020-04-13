1bd3553334e9743a50f06a0d3620ec9058ff33e6
André Lins <andrelucas01@hotmail.com>
2020-04-13 12:13:03 -0300

    New example screen

--- src/dev/App.svelte
+++ src/dev/App.svelte
@@ -9,22 +9,13 @@
     flex-flow: column wrap;
     justify-content: center;
     width: 100%;
-  }
-
-  div {
-    align-items: center;
-    background-color: #417b5a;
-    display: flex;
-    flex-flow: column wrap;
-    height: 120px;
-    justify-content: center;
-    margin: 10px;
-    width: 300px;
+    min-height: 100%;
   }
 
   h1 {
     color: #1f2041;
     text-transform: uppercase;
+    margin-bottom: 0px;
   }
 
   img {
@@ -32,30 +23,66 @@
     width: 50%;
   }
 
-  .buttonImage {
-    border: none;
-    background: transparent;
+  .root {
+    width: 100%;
+    display: flex;
+    justify-content: center;
   }
 
-  .buttonImage:focus {
-    outline: none;
+  .card {
+    box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
+      0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
+    border-radius: 2px;
+    width: 400px;
+    min-width: 40%;
+    max-width: 40%;
+    background-color: white;
+    box-sizing: border-box;
+    min-height: 400px;
+    max-height: 400px;
+    overflow-x: scroll;
+    padding: 16px;
+    box-sizing: border-box;
+    margin-right: 40px;
+  }
+
+  .card:last-child {
+    margin-right: 0px;
+  }
+  .container-fullscreen {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #f9a825;
+  }
+
+  img {
+    width: 100%;
+    height: auto;
   }
 </style>
 
 <main>
   <h1>Svelte Fullscreen</h1>
-  <Fullscreen let:onRequest let:onExit>
-    <div>
-      <button on:click={() => onRequest()}>FullScreen</button>
-      <button on:click={() => onExit()}>Screen</button>
+  <h4>Component that performs fullscreen in DOM Elements</h4>
+  <div class="root">
+    <div class="card">
+      <Fullscreen let:onRequest let:onExit>
+        <div class="container-fullscreen">
+          <button on:click={() => onRequest()}>FullScreen</button>
+          <button on:click={() => onExit()}>Screen</button>
+        </div>
+      </Fullscreen>
     </div>
-  </Fullscreen>
-
-  <Fullscreen let:onToggle on:change={() => console.log('change')}>
-    <button class="buttonImage" on:click={() => onToggle()}>
-      <img
-        alt="Fullscreen"
-        src="https://images.pexels.com/photos/34950/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2" />
-    </button>
-  </Fullscreen>
+    <div class="card">
+      <Fullscreen let:onToggle on:change={() => console.log('change')}>
+        <img
+          on:click={() => onToggle()}
+          alt="Fullscreen"
+          src="https://spguia.melhoresdestinos.com.br/system/fotos_local/fotos/26013/show/praia-do-aventureiro.jpg" />
+      </Fullscreen>
+    </div>
+  </div>
 </main>
