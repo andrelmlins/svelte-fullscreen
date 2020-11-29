d9453379ccd2e92da3d3b0a7dd8dab54b71389d5
André Lins <andrelucas01@hotmail.com>
2020-11-28 23:56:08 -0300

    Fix rollup setting

--- rollup.config.js
+++ rollup.config.js
@@ -14,40 +14,36 @@ export default [
       sourcemap: true,
       format: "iife",
       name: "app",
-      file: "public/build/bundle.js"
+      file: "public/build/bundle.js",
     },
     plugins: [
       svelte({
         dev: !production,
-        css: css => {
-          css.write("public/build/bundle.css");
-        }
+        css: (css) => css.write("bundle.css"),
       }),
       resolve({
         browser: true,
-        dedupe: importee =>
-          importee === "svelte" || importee.startsWith("svelte/")
+        dedupe: (importee) =>
+          importee === "svelte" || importee.startsWith("svelte/"),
       }),
       commonjs(),
       !production && serve(),
       !production && livereload("public"),
-      production && terser()
+      production && terser(),
     ],
-    watch: {
-      clearScreen: false
-    }
+    watch: { clearScreen: false },
   },
   {
     input: "src/lib/Fullscreen.svelte",
     output: { file: pkg.main, format: "umd", name: "Fullscreen" },
-    plugins: [svelte(), resolve(), commonjs()]
+    plugins: [svelte(), resolve(), commonjs()],
   },
   {
     input: "src/lib/Fullscreen.svelte",
     output: { file: pkg.module, format: "es" },
     external: ["svelte/internal"],
-    plugins: [svelte(), commonjs()]
-  }
+    plugins: [svelte(), commonjs()],
+  },
 ];
 
 function serve() {
@@ -60,9 +56,9 @@ function serve() {
 
         require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
           stdio: ["ignore", "inherit", "inherit"],
-          shell: true
+          shell: true,
         });
       }
-    }
+    },
   };
 }
